4ec40fb4179d0ecaeb5bd0e4d63f653b27daba4e
Ken <>
2021-12-22 10:40:13 +0700

    viewmodel scope for use case

--- core/src/main/java/net/fitken/core/di/UseCaseModule.kt
+++ core/src/main/java/net/fitken/core/di/UseCaseModule.kt
@@ -3,34 +3,34 @@ package net.fitken.core.di
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 import net.fitken.core.domain.repository.MapRepository
 import net.fitken.core.domain.repository.MovieRepository
 import net.fitken.core.domain.usecase.GetDirectionUseCase
 import net.fitken.core.domain.usecase.GetMovieDetailsUseCase
 import net.fitken.core.domain.usecase.GetNowPlayingUseCase
 import net.fitken.core.domain.usecase.GetTopRatedUseCase
-import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 object UseCaseModule {
-    @Singleton
+    @ViewModelScoped
     @Provides
     fun provideGetNowPlayingUseCase(movieRepository: MovieRepository): GetNowPlayingUseCase =
         GetNowPlayingUseCase(movieRepository)
 
-    @Singleton
+    @ViewModelScoped
     @Provides
     fun provideGetTopRatedUseCase(movieRepository: MovieRepository): GetTopRatedUseCase =
         GetTopRatedUseCase(movieRepository)
 
-    @Singleton
+    @ViewModelScoped
     @Provides
     fun provideGetMovieDetailsUseCase(movieRepository: MovieRepository): GetMovieDetailsUseCase =
         GetMovieDetailsUseCase(movieRepository)
 
-    @Singleton
+    @ViewModelScoped
     @Provides
     fun provideGetMapDirectionUseCase(mapRepository: MapRepository): GetDirectionUseCase =
         GetDirectionUseCase(mapRepository)
